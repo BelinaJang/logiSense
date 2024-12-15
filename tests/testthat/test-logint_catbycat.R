000cfb2f37b5ac274ebe8ff567bb7503063fc20a
vnssyl <vanessa.y.liao@gmail.com>
2024-12-15 13:16:25 -0500

    test file for cat by cat interaction fcn

--- tests/testthat/test-logint_catbycat.R
+++ tests/testthat/test-logint_catbycat.R
@@ -1,3 +1,26 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("logint_catbycat handles interaction between two categorical variables correctly", {
+  formula <- stroke ~ work_type * Residence_type
+  variable1 <- "work_type"
+  variable2 <- "Residence_type"
+  data <- test_data
+  sigfig <- 4
+
+  expect_output(
+    logint_catbycat(formula, variable1,variable2, data, sigfig),
+    "The odds ratio for the outcome",
+    fixed = TRUE
+  )
+})
+
+test_that("logint_catbycat throws an error for missing interaction term", {
+  formula <- stroke ~ age*work_type + Residence_type
+  variable1 <- "work_type"
+  variable2 <- "Residence_type"
+  data <- test_data
+  sigfig <- 4
+
+  expect_error(
+    logint_contbycat(formula, variable1,variable2, data, sigfig),
+    "No interaction terms between work_type and Residence_type found in the model."
+  )
 })
