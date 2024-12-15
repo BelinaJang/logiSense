e37b1ee0bd0b5817daa53088b9b6614b25811cb8
BelinaJang <89424860+BelinaJang@users.noreply.github.com>
2024-12-15 13:57:08 -0500

    add more test

--- tests/testthat/test-logint_catbycat.R
+++ tests/testthat/test-logint_catbycat.R
@@ -12,6 +12,20 @@ test_that("logint_catbycat handles interaction between two categorical variables
   )
 })
 
+test_that("logint_catbycat handles interaction between two categorical variables correctly even when the two variables are not in the right order", {
+  formula <- stroke ~ work_type * Residence_type
+  variable1 <- "Residence_type"
+  variable2 <- "work_type"
+  data <- test_data
+  sigfig <- 4
+
+  expect_output(
+    logint_catbycat(formula, variable1,variable2, data, sigfig),
+    "The odds ratio for the outcome",
+    fixed = TRUE
+  )
+})
+
 test_that("logint_catbycat throws an error for missing interaction term", {
   formula <- stroke ~ age*work_type + Residence_type
   variable1 <- "work_type"
