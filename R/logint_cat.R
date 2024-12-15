63c8415fe999b43140dea677b8bdf514e6d80166
vnssyl <vanessa.y.liao@gmail.com>
2024-12-14 19:04:09 -0500

    OR and CI calculation, interpretation

--- R/logint_cat.R
+++ R/logint_cat.R
@@ -19,7 +19,6 @@ logint_catbycat <- function(formula, variable1, variable2, variable1_type, varia
 
 
 # # assigning variable1 to appropriate type variable
-  }
 
 
   # assigning variable1 to appropriate type variable
@@ -51,8 +50,10 @@ logint_catbycat <- function(formula, variable1, variable2, variable1_type, varia
     var2_effect <- coefficients[categorical_var2]
     interaction_effect <- coefficients[grep(":", names(coefficients))]
 
-        result <- tidy(model)
+    result <- tidy(model)
     interaction_terms <- coefficients[result[grep(":", result$term), ]$term]
+
+    intercept <- coefficients[1]
 
     cat_effects1 <- coefficients[grep(paste0("^", categorical_var1), names(coefficients))]
     cat_effects2 <- coefficients[grep(paste0("^", categorical_var2), names(coefficients))]
@@ -60,5 +61,63 @@ logint_catbycat <- function(formula, variable1, variable2, variable1_type, varia
     cat_levels1 <- levels(as.factor(model$model[[categorical_var1]]))
     cat_levels2 <- levels(as.factor(model$model[[categorical_var2]]))
 
+    # OR(s)
+    vcov_m <- vcov(model)
+    odds_ratios <- c(exp(intercept), exp(intercept + interaction_terms))
+    odds_ratios_names <- names(odds_ratios)
+    odds_ratio_sentences <- c()
+
+    for (i in seq_along(interaction_terms)) {
+      # 95% confint
+      var <- vcov_m[odds_ratios_names[i], odds_ratios_names[i]]
+      se <- sqrt(var)
+      ci <- exp(base_effect + c(-1,1) * 1.96 * se)
+
+      # sentences
+      odds_ratio_sentences <- c(
+        odds_ratio_sentences,
+        paste0(
+          "The odds ratio for the outcome '", outcome, "' when comparing the group ",
+          categorical_var1," level '", cat_levels1[i], "' with the reference level, ",
+          "in the context of ", categorical_var2, " level '", cat_levels2[i],
+          "' is", signif(odds_ratios[i], sigfig), "."
+        ),
+        paste0(
+          "The 95% CI for this odds ratio is: [", signif(CI[1], sigfig),
+          ", ", signif(CI[2], sigfig),"].\n"
+        )
+      )
+    }
+
   }
+}
+
+
+
+######## tests OR(s)
+vcov_m <- vcov(model)
+odds_ratios <- c(exp(intercept), exp(intercept + interaction_terms))
+odds_ratios_names <- names(odds_ratios)
+odds_ratio_sentences <- c()
+
+for (i in seq_along(interaction_terms)) {
+  # 95% confint
+  var <- vcov_m[odds_ratios_names[i], odds_ratios_names[i]]
+  se <- sqrt(var)
+  ci <- exp(intercept + c(-1,1) * 1.96 * se)
+
+  # sentences
+  odds_ratio_sentences <- c(
+    odds_ratio_sentences,
+    paste0(
+      "The odds ratio for the outcome '", outcome, "' when comparing the group ",
+      categorical_var1," level '", cat_levels1[i], "' with the reference level, ",
+      "in the context of ", categorical_var2, " level '", cat_levels2[i],
+      "' is ", signif(odds_ratios[i], sigfig), "."
+    ),
+    paste0(
+      "The 95% CI for this odds ratio is: [", signif(ci[1], sigfig),
+      ", ", signif(ci[2], sigfig),"].\n"
+    )
+  )
 }
